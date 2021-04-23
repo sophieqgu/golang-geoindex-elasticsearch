1cd68464978c058cfe4091296173afce7be56699
sophieqgu <sophieqgu@gmail.com>
2021-04-23 18:15:12 +0000

    Add handler for signup and login

--- user.go
+++ user.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"reflect"
 	"regexp"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/olivere/elastic"
 )
 
@@ -56,4 +61,88 @@ func addUser(user *User) (bool, error) {
 	}
 	fmt.Printf("User is added: %s\n", user.Username)
 	return true, nil
+}
+
+func handlerLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one login request")
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	//  Get User information from client
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	if err := decoder.Decode(&user); err != nil {
+		http.Error(w, "Cannot decode user data from client", http.StatusBadRequest)
+		fmt.Printf("Cannot decode user data from client %v\n", err)
+		return
+	}
+
+	exists, err := checkUser(user.Username, user.Password)
+	if err != nil {
+		http.Error(w, "Failed to read user from Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read user from Elasticsearch %v\n", err)
+		return
+	}
+
+	if !exists {
+		http.Error(w, "User doesn't exists or wrong password", http.StatusUnauthorized)
+		fmt.Printf("User doesn't exists or wrong password\n")
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(mySigningKey)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		fmt.Printf("Failed to generate token %v\n", err)
+		return
+	}
+
+	w.Write([]byte(tokenString))
+}
+
+func handlerSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one signup request")
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	if err := decoder.Decode(&user); err != nil {
+		http.Error(w, "Cannot decode user data from client", http.StatusBadRequest)
+		fmt.Printf("Cannot decode user data from client %v\n", err)
+		return
+	}
+
+	if user.Username == "" || user.Password == "" || regexp.MustCompile(`^[a-z0-9]$`).MatchString(user.Username) {
+		http.Error(w, "Invalid username or password", http.StatusBadRequest)
+		fmt.Printf("Invalid username or password\n")
+		return
+	}
+
+	success, err := addUser(&user)
+	if err != nil {
+		http.Error(w, "Failed to save user to Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to save user to Elasticsearch %v\n", err)
+		return
+	}
+
+	if !success {
+		http.Error(w, "User already exists", http.StatusBadRequest)
+		fmt.Println("User already exists")
+		return
+	}
+	fmt.Printf("User added successfully: %s.\n", user.Username)
 }
